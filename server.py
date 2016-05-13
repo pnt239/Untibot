8792eb97815730198021c33aba07b87443e40824
panoti <pano.tjoan@yahoo.com>
2016-05-13 23:48:28 +0700

    show gray

--- server.py
+++ server.py
@@ -115,10 +115,10 @@ class WebSocket(tornado.websocket.WebSocketHandler):
 
             t = clock()
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            gray = cv2.equalizeHist(gray)
+            #gray = cv2.equalizeHist(gray)
 
             #rects = detect(gray, cascade)
-            vis = img.copy()
+            vis = gray.copy()
             #draw_rects(vis, rects, (0, 255, 0))
             #if not self.nested.empty():
             #    for x1, y1, x2, y2 in rects:
