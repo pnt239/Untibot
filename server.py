1015f946d01eea8f75166d84a6effed5caacaf18
panoti <pano.tjoan@yahoo.com>
2016-05-14 00:58:30 +0700

    try with ver2

--- server.py
+++ server.py
@@ -165,7 +165,7 @@ else:
     camera = picamera.PiCamera()
     camera.start_preview()
 
-fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+fgbg = cv2.bgsegm.createBackgroundSubtractorMOG2()
 
 resolutions = {"high": (1280, 720), "medium": (640, 480), "low": (320, 240), "360" : (480, 360)}
 if args.resolution in resolutions:
